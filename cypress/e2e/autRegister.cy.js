ae14e069daf0d8989ec885d272acf3ab5beefca2
Kira <kira.oliveira.web@gmail.com>
2023-03-23 23:04:44 -0300

    Finalizando automação do formulário

--- cypress/e2e/autRegister.cy.js
+++ cypress/e2e/autRegister.cy.js
@@ -43,6 +43,65 @@ describe('Acessar site', () => {
       //Validando o campo "email"
       cy.get('#email').should('exist').and('be.visible')
       cy.get('#email').type('kira@gmail.com')
+
+      //Aqui ele valida se a frase "De qual lado da aplicação desenvolve?" aparece e se label "Back-end" existe e está visível
+      cy.contains('De qual lado da aplicação desenvolve?')
+      cy.contains('Back-end')
+      cy.get(':nth-child(3) > :nth-child(3)').should('exist').and('be.visible')
+      cy.get(':nth-child(3) > :nth-child(3)').click()
+
+      //Aqui ele valida se a label "Senioridade" existe e está visível
+      cy.contains('Senioridade')
+      cy.get('#senioridade').should('exist').and('be.visible')
+      cy.get('#senioridade').select('Pleno')
+
+      //Aqui ele valida se a frase "Selecione as tecnologias que utiliza:" aparece
+      cy.contains('Selecione as tecnologias que utiliza:')
+
+      //Após validar a frase, ele valida se as labels que serão marcadas aparecem
+
+      //Aguardar 2 segundos para poder ver o teste
+      cy.wait(2000)
+
+      //CSS - Será marcado e desmarcado
+      cy.contains('CSS')
+      cy.get('#tecnologia2').should('exist').and('be.visible')
+      cy.get('#tecnologia2').check().uncheck()
+
+      //Aguardar 2 segundos para poder ver o teste
+      cy.wait(2000)
+
+      //JavaScript - Será marcado e desmarcado
+      cy.contains('JavaScript')
+      cy.get('#tecnologia3').should('exist').and('be.visible')
+      cy.get('#tecnologia3').check().uncheck()
+
+      //Aguardar 2 segundos para poder ver o teste
+      cy.wait(2000)
+
+      //Java - Será marcado e desmarcado
+      cy.contains('Java')
+      cy.get('#tecnologia7').should('exist').and('be.visible')
+      cy.get('#tecnologia7').check().uncheck()
+
+      //Aguardar 2 segundos para poder ver o teste
+      cy.wait(2000)
+
+      //Python - Será marcado e permanecerá marcado
+      cy.contains('Python')
+      cy.get('#tecnologia6').should('exist').and('be.visible')
+      cy.get('#tecnologia6').check()
+
+      //Aqui valida o campo "Experiência" e insere dados
+      cy.contains('Conte um pouco da sua experiência:')
+      cy.get('#experiencia').should('exist').and('be.visible')
+      cy.get('#experiencia').type('6 anos na área de QA')
+
+      cy.screenshot('Full Screen')
+
+      //Aqui valida o botão e clica nele
+      cy.get('.botao').should('exist').and('be.visible')
+      cy.get('.botao').click()
     })
   })
 })
